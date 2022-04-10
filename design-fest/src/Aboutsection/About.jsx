7fc1ee73ac8282206076e2317b017f4d276df71a
saiddrai <saiddrai42@gmail.com>
2022-04-10 23:51:36 +0100

    About section structure

--- design-fest/src/Aboutsection/About.jsx
+++ design-fest/src/Aboutsection/About.jsx
@@ -5,25 +5,34 @@ import Details from "./details/details";
 export default function About(){
     return(
         <div className="about">
-            <div>
-                <h2>
-                    ABOUT DESIGN FEST
-                </h2>
-                <Button name = "REGISTER IN THE EVENT"/>
-            </div>
-            <div>
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, vel omnis repellendus mollitia, explicabo, maiores quisquam numquam quia reiciendis sit, accusantium atque ex animi perspiciatis ab odit earum assumenda aliquid.
-                </p>
 
-                <p>
-                Dolor sit amet, consectetur adipisicing elit. Commodi, vel omnis repellendus mollitia, explicabo, maiores quisquam numquam quia reiciendis sit, accusantium atque ex animi perspiciatis ab odit earum assumenda aliquid.
-                </p>
-                <div>
-                    <Details number = "03" text = "DAYS EVENT"/>
-                    <Details number = " 10 +" text = "CONFRENCES"/>
-                    <Details number = " 20 +" text = "WORKSHOPS"/>
+            <div className="left">
+                    <h2> ABOUT DESIGN FEST</h2>
+                    <Button name = "REGISTER IN THE EVENT"/>
+            </div>
+
+            <div className="right">
+                <div className="paragraphs">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, vel omnis repellendus mollitia, explicabo, maiores quisquam numquam quia reiciendis sit, accusantium atque   ex animi perspiciatis ab odit earum assumenda aliquid.
+                    </p>
+
+                    <p>
+                    Dolor sit amet, consectetur adipisicing elit. Commodi, vel omnis repellendus mollitia, explicabo, maiores quisquam numquam quia reiciendis sit, accusantium atque ex animi  perspiciatis ab odit earum assumenda aliquid.
+                    </p>
                 </div>
+                    <div className="detailsLine">
+                        <Details number = "03" text = "DAYS EVENT"/>
+                        <Details number = " 10 +" text = "CONFRENCES"/>
+                        <Details number = " 20 +" text = "WORKSHOPS"/>
+                    </div>
+
+            </div>
+
+
+            <div className="row">
+
+                
             </div>
         </div>
     )
--- design-fest/src/Aboutsection/details/details.jsx
+++ design-fest/src/Aboutsection/details/details.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import './details'
 
 export default function Details(props){
     return(
-        <div>
+        <div className="details">
             <h3>{props.number}</h3>
             <h6> {props.text} </h6>
         </div>
